69a3b7f863371692458dfb9f5eedded357315602
lizad <lizador0397@gmail.com>
2023-02-26 19:52:25 +0300

    commit

---
+++ src/stepic_java_liza/transport.java
@@ -0,0 +1,29 @@
+package stepic_java_liza;
+
+public class transport {
+    public static void main(String[] args) {
+
+    }
+    interface Military{
+
+    }
+
+    interface PublicTransport{
+
+    }
+
+    class Transport {
+
+    }
+    public class Train extends Transport implements PublicTransport{
+
+    }
+
+    public class Bus extends Transport implements PublicTransport{
+
+    }
+    public class Car extends  Transport implements PublicTransport{
+
+    }
+
+}
