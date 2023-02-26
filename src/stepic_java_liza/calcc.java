22221584a930b0113db4349d45829aeee1639d4f
lizad <lizador0397@gmail.com>
2023-02-26 20:30:52 +0300

    commit

--- src/stepic_java_liza/calcc.java
+++ src/stepic_java_liza/calcc.java
@@ -2,27 +2,31 @@ package stepic_java_liza;
 
 
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.io.File;
 public class calcc{
     public static void main(String[] args) throws FileNotFoundException {
     File file = new File("C:\\Users\\lizad\\IdeaProjects\\stepic_liza\\src\\stepic_java_liza\\inputt.txt");
+    PrintWriter printWriter = new PrintWriter("C:\\Users\\lizad\\IdeaProjects\\stepic_liza\\src\\stepic_java_liza\\output.txt");
+
     Scanner sc = new Scanner(file);
         try {
             Double x = Double.parseDouble(sc.next());
             String sym = sc.next();
             Double y = Double.parseDouble(sc.next());
             if (sym.equals("+")) {
-                System.out.println(x + y);
+                printWriter.println(x + y);
+                printWriter.close();
             } else if (sym.equals("-")) {
-                System.out.println(x - y);
+                printWriter.println(x - y);
             } else if (sym.equals("*")) {
-                System.out.println(x * y);
+                printWriter.println(x * y);
             } else if (sym.equals("/")) {
                 if (y == 0) {
                     throw new ArithmeticException();
                 } else {
-                    System.out.println(x / y);
+                    printWriter.println(x / y);
                 }
             } else {
                 throw new UnsupportedOperationException();
