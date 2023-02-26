1431699ef77f89fc410fe9d878e119ce2bc06220
lizad <lizador0397@gmail.com>
2023-02-26 19:38:31 +0300

    commit

--- src/stepic_java_liza/Main.java
+++ src/stepic_java_liza/Main.java
@@ -4,4 +4,26 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    class Fig{
+    }
+    class Triangle extends Fig{}
+    class Triangle_isosceles extends Triangle{}
+    class Triangle_equals extends Triangle{}
+    class Triangle_right extends Triangle{}
+
+    class Quadrilateral  extends Fig{}
+
+    class Convex_quadrilateral  extends Quadrilateral{}
+    class Parallelogram  extends Quadrilateral{}
+    class Rectangle  extends Quadrilateral{}
+    class Square  extends Quadrilateral{}
+    class Rhomb  extends Quadrilateral{}
+    class Trapezoid  extends Quadrilateral{}
+
+    class Pentagon  extends Fig{}
+    class Hexagon  extends Fig{}
+
+    class Circle  extends Fig{}
+    class Oval  extends Circle{}
+
 }
